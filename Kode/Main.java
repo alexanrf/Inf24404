dbac3bc883e913b8420834a8ade7fac964e5d999
Alexander <alexander.fife@outlook.com>
2016-05-21 06:18:41 +0200

    Add files via upload

--- Kode/Main.java
+++ Kode/Main.java
@@ -5,7 +5,8 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        //QuickHullPara qHull = new QuickHullPara(100, 1, true);
+        //QuickHullPara qHull = new QuickHullPara(50, 1, true);
+        //qHull.findHull();
         runTests();
     }
 
@@ -13,18 +14,35 @@ public class Main {
         int threadCount = Runtime.getRuntime().availableProcessors();
         System.out.println("Cores available: " + threadCount);
         int tests = 9;
-        int[] n = {100, 1000, 10000, 100000, 1000000, 10000000, 70000000};
+        int[] n = {100, 1000, 10000, 100000, 1000000, 10000000, 50000000};
+
         for(int i = 0; i < n.length; i++){
-            System.out.print("--- n: " +  n[i] + " ---");
+            System.out.println("n   tidSekv     tidPara     speedup");
             for(int t = 0; t < tests; t++){
-                QuickHullPara qHull = new QuickHullPara(n[i],threadCount, false); //Change threadCount to 1 if you want to run sequentially
-                qHull.findHull();
-                System.out.print("Test: "+ t + " - ");
+                QuickHullPara qHullSekv = new QuickHullPara(n[i], 1, false);
+                double tidSekv = qHullSekv.findHull();
+                tidSekv = Math.floor(tidSekv * 100) / 100;
+                System.gc(); //Makes the times a loooooot more normalized
+
+                QuickHullPara qHullPara = new QuickHullPara(n[i],threadCount, false); //Change threadCount to 1 if you want to run sequentially
+                double tidPara = qHullPara.findHull();
+                tidPara = Math.floor(tidPara * 100) / 100;
+                System.gc();
+
+                double speedUp = tidSekv / tidPara;
+                speedUp = Math.floor(speedUp * 100) / 100;
+
+                if(qHullSekv.hull.size() != qHullPara.hull.size()){
+                    System.out.println("HULL ERROR - Sekv.size() = " + qHullSekv.hull.size() + " - Para.size() = " + qHullPara.hull.size());
+                }
+
+                System.out.println(n[i] + "\t" + tidSekv + "ms\t" + tidPara + "ms\t" + speedUp);
             }
         }
 
     }
 }
+
 
 class QuickHullPara{
     int n;
@@ -33,6 +51,7 @@ class QuickHullPara{
     NPunkter p;
     int threadCount;
     boolean draw;
+    IntList hull;
 
     QuickHullPara(int n, int threadCount, boolean draw){
         this.n = n;
@@ -42,6 +61,7 @@ class QuickHullPara{
         p.fyllArrayer(x, y);
         this.threadCount = threadCount;
         this.draw = draw;
+        this.hull = new IntList();
     }
 
     //Left, right, Point we want to check
@@ -55,14 +75,12 @@ class QuickHullPara{
             return -1;
     }
 
-    void findHull(){
-
-        IntList hull = new IntList();
-        IntList onPoint = new IntList();
+    double findHull(){
+        long t = System.nanoTime(); //Starts clock
 
         //Find smallest and largest points on the X axis
         int minP = -1, maxP = -1;
-        int minX = x[0], maxX = x[0];
+        int minX = Integer.MAX_VALUE, maxX = Integer.MIN_VALUE;
 
         for(int i = 0; i < x.length; i++){
             if(x[i] < minX){
@@ -70,16 +88,16 @@ class QuickHullPara{
                     minX = x[i];
                     minP = i;
                 }
-                else if(y[minP] > y[i]){
+                else if(y[minP] >= y[i]){
                     minX = x[i];
                     minP = i;
                 }
             }
-            if(x[i] > maxX) {
+            else if(x[i] > maxX) {
                 if (maxP == -1) {
                     maxX = x[i];
                     maxP = i;
-                } else if (y[maxP] < y[i]) {
+                } else if (y[maxP] <= y[i]) {
                     maxX = x[i];
                     maxP = i;
                 }
@@ -100,13 +118,13 @@ class QuickHullPara{
         }
 
 
-        long t = System.nanoTime(); //Starts clock
+
         if(threadCount == 1){ //1 core on CPU
             hull.add(minP);
-            HullSet hullSet = new HullSet(hull, rightPoints, minP, maxP, 0, onPoint);
+            HullSet hullSet = new HullSet(hull, rightPoints, minP, maxP, 0, new IntList());
             hullSet.run();
             hull.add(maxP);
-            hullSet = new HullSet(hull, leftPoints, maxP, minP, 0, onPoint);
+            hullSet = new HullSet(hull, leftPoints, maxP, minP, 0, new IntList());
             hullSet.run();
 
         }
@@ -119,16 +137,15 @@ class QuickHullPara{
             }
 
             //Starts up 2 threads
-            IntList onPoint2 = new IntList();
             level--;
             IntList hull1 = new IntList();
-            HullSet hullSet1 = new HullSet(hull1, rightPoints, minP, maxP, level, onPoint);
+            HullSet hullSet1 = new HullSet(hull1, rightPoints, minP, maxP, level, new IntList());
             hullSet1.start();
             IntList hull2 = new IntList();
-            HullSet hullSet2 = new HullSet(hull2, leftPoints, maxP, minP, level, onPoint2);
+            HullSet hullSet2 = new HullSet(hull2, leftPoints, maxP, minP, level, new IntList());
             hullSet2.start();
 
-            try{ //Waits for them to finish, then adds the hulls together, and the onPoints.
+            try{ //Waits for them to finish, then adds the hulls together
                 hullSet1.join();
                 for(int i = 0; i < hullSet1.hull.size(); i++){
                     hull.add(hullSet1.hull.get(i));
@@ -138,34 +155,24 @@ class QuickHullPara{
                 for(int i = 0; i < hullSet2.hull.size(); i++){
                     hull.add(hullSet2.hull.get(i));
                 }
-                for(int i = 0; i < onPoint2.size(); i++){
-                    onPoint.add(onPoint2.get(i));
-                }
             }catch(Exception e){
                 e.printStackTrace();
             }
         }
 
         double time = (System.nanoTime() - t) / 1000000.0; //Ends clock
-        System.out.println("Time to run: " + time);
 
         if(draw){ //Draws the graph if constructor argument is true
             Oblig4 o = new Oblig4(x, y, n, hull, p);
             o.draw();
         }
+        return time;
 
-
-        //Prints out info about the hull
-        //System.out.println("Found: " + (hull.size() + onPoint.size()) + " points in the convex hull (" + onPoint.size()+" unsorted) where n = " + n);
-
-        /* Prints out the points
+        /* Prints out the points - Doesn't make sense doing this when taking the time
         for(int i = 0; i < hull.size(); i++){
             System.out.println("P: " + hull.get(i) + " - X: " + x[hull.get(i)] + " - Y: " + y[hull.get(i)]);
         }
-        System.out.println("--- Unsorted Points ---");
-        for(int i = 0; i < onPoint.size(); i++){
-            System.out.println("P: " + onPoint.get(i) + " - X: " + x[onPoint.get(i)] + " - Y: " + y[onPoint.get(i)]);
-        }
+        System.out.println("HULL SIZE: " + hull.size());
         */
     }
 
@@ -176,7 +183,7 @@ class QuickHullPara{
 
     private class HullSet extends Thread{
         int level;
-        IntList hull, pointSet, onPoint;
+        IntList hull, pointSet, onPoint, onPointLeft;
         int a, b;
 
         HullSet(IntList hull, IntList pointSet, int a, int b, int level, IntList onPoint){
@@ -186,13 +193,21 @@ class QuickHullPara{
             this.b = b;
             this.level = level;
             this.onPoint = onPoint;
+            this.onPointLeft = new IntList();
         }
 
         @Override
         public void run(){
-            if(pointSet.size() == 0){ //No more points - Exit
+            //No more points - Exit
+            if(pointSet.size() == 0){
                 return;
             }
+
+            //Clears the onPoints, as they aren't on the hull.
+            onPoint.clear();
+            onPointLeft.clear();
+
+
 
             //Find point furthest away
             int dist = -1, furthestPoint = -1;
@@ -204,27 +219,10 @@ class QuickHullPara{
                 }
             }
 
-            if(pointSet.size() == 1){
-                hull.add(pointSet.get(0)); //Only 1 point - Add to set and exit
-                return;
-            }
-
-            //Find the points to the right of A to C (Right set)
-            //Then find the points the the left of B to C (left set)
-            //Left/Right is such an abstract concept when applied here. Names could be wrong, but it works
-
             IntList rightPoints = new IntList();
             IntList leftPoints = new IntList();
-            //Below was an attempt at trying to get the onPoints in the hull set in the correct order.
-            //I wasn't able to make it work.
-            //My idea was to sort the values on the distance from a/b. And add them together at the end.
-            //Didn't find anything that worked
-            //Made an unsorted list instead
-            //TreeMap<Double,Integer> tmRight = new TreeMap<Double,Integer>();
-            //TreeMap<Double,Integer> tmLeft = new TreeMap<Double,Integer>();
 
             //Finds out which side the points are on (or on the line)
-            //There could be a small inefficiency on the ifs here. But not much compared to the math.
             for(int i = 0; i < pointSet.size(); i++){
                 int point = pointSet.get(i);
                 int sideInt = pointSide(a, furthestPoint, point);
@@ -236,57 +234,71 @@ class QuickHullPara{
                     leftPoints.add(point);
                 }
 
-                else if((sideInt == 0 || sideInt2 == 0) && (point != a && point != b && point != furthestPoint)){
+                else if((sideInt == 0) && (point != a && point != b && point != furthestPoint)){
                     onPoint.add(point);
-                    //double distance = Math.sqrt(Math.abs((double)(x[point] - x[a]) + (y[point] - y[a])));
-                    //tmRight.put(distance, point);
                 }
-                /*
-                else if(sideInt2 == 0 && (point != a && point != b && point != furthestPoint)){
-                    //double distance = Math.sqrt(Math.abs((double)(x[point] - x[a]) + (y[point] - y[a])));
-                    //tmLeft.put(distance, point);
+                else if((sideInt2 == 0) && (point != a && point != b && point != furthestPoint)){
+                    onPointLeft.add(point);
                 }
-                */
-
             }
 
 
             if(level < 1){//Sequential
                 HullSet hullSet = new HullSet(hull, rightPoints, a, furthestPoint, level, onPoint);
                 hullSet.run();
+                addSortedToHull(hull, onPoint);
                 hull.add(furthestPoint);
-                hullSet = new HullSet(hull, leftPoints, furthestPoint,b, level, onPoint);
+                hullSet = new HullSet(hull, leftPoints, furthestPoint,b, level, onPointLeft);
                 hullSet.run();
+                addSortedToHull(hull, onPointLeft);
             }
             else{ //Parallell
                 level--; //We go down a level
                 IntList hull1 = new IntList(); //Two new hull sets, one for each side.
-                IntList onPoint1 = new IntList(); //New on Point as well.
-                HullSet hullSet1 = new HullSet(hull1, rightPoints, a, furthestPoint, level, onPoint1);
+                HullSet hullSet1 = new HullSet(hull1, rightPoints, a, furthestPoint, level, onPoint);
                 hullSet1.start();
                 IntList hull2 = new IntList();
-                HullSet hullSet2 = new HullSet(hull2, leftPoints, furthestPoint,b, level, onPoint);
+                HullSet hullSet2 = new HullSet(hull2, leftPoints, furthestPoint,b, level, onPointLeft);
                 hullSet2.run(); //I don't make a new thread here, this nodes thread goes down to the other set instead of waiting for 2 threads to finish.
 
                 try{
                     hullSet1.join();
+                    addSortedToHull(hull, onPoint);
                     for(int i = 0; i < hullSet1.hull.size(); i++){
                         hull.add(hullSet1.hull.get(i));
                     }
                     hull.add(furthestPoint);
-                    for(int i = 0; i < onPoint1.size(); i++){
-                        onPoint.add(onPoint1.get(i));
-                    }
 
                     for(int i = 0; i < hullSet2.hull.size(); i++){
                         hull.add(hullSet2.hull.get(i));
                     }
+                    addSortedToHull(hull, onPointLeft);
 
                 }catch(Exception e){
                     e.printStackTrace();
                 }
             }
         }
+
+        void addSortedToHull(IntList hull, IntList onPoint){
+            if(onPoint.size() == 1){
+                hull.add(onPoint.get(0));
+            }else if(onPoint.size() > 1){
+                //Sorts the points in a sorted container
+                TreeMap<Double,Integer> sorted = new TreeMap<Double,Integer>();
+                for(int i = 0; i < onPoint.size(); i++){
+                    int point = onPoint.get(i);
+                    double distance = Math.sqrt(Math.abs((double)(x[point] - x[a]) + (y[point] - y[a])));
+                    sorted.put(distance, point);
+                }
+                //Adds them sorted to the hull
+                for(Map.Entry<Double,Integer> entry : sorted.entrySet()) {
+                    hull.add(entry.getValue());
+                }
+            }
+            onPoint.clear();
+        }
+
     }
 }
 
